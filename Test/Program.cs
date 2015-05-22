6035f748d74361375de8a4c632a91aaa886cefe9
kevinsmgov <kevin.hobson@smgov.net>
2015-05-22 14:58:06 -0700

    testing virtual tables blocks and shapes

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,14 +11,34 @@ namespace Test
         static void Main(string[] args)
         {
             var gtfs = new GTFSTools.IO.GTFS("test.zip");
-            foreach (var trip in gtfs.DataSet._trips_txt)
+            foreach (var block in gtfs.DataSet.blocks)
             {
-                Console.WriteLine("{0}, {1}", trip.trip_id, trip._Getstop_times_txtRows().Count());
-                //foreach (var stop_time in trip._Getstop_times_txtRows())
-                //{
-                //    Console.WriteLine("{0}, {1}", stop_time.stop_sequence, stop_time.Isarrival_timeNull() ? (TimeSpan?)null : stop_time.arrival_time);
-                //}
+                Console.WriteLine("{0}", block.block_id);
+                foreach (var trip in block._Gettrips_txtRows().OrderBy(item => item._Getstop_times_txtRows().Where(stop_time => !stop_time.Isarrival_timeNull()).Min(stop_time => stop_time.arrival_time)))
+                {
+                    Console.WriteLine("\t{0}, {1}", trip.trip_id, trip._Getstop_times_txtRows().Count());
+                    foreach (var stop_time in trip._Getstop_times_txtRows())
+                    {
+                        Console.WriteLine("\t\t{0}, {1}", stop_time.stop_sequence, stop_time.Isarrival_timeNull() ? (TimeSpan?)null : stop_time.arrival_time);
+                    }
+                    foreach (var path in trip.pathsRow._Getshapes_txtRows())
+                    {
+                        Console.WriteLine("\t\t{0}, {1}, {2}", path.shape_pt_sequence, path.shape_pt_lat, path.shape_pt_lon);
+                    }
+                }
             }
+            //foreach (var trip in gtfs.DataSet._trips_txt)
+            //{
+            //    //Console.WriteLine("{0}, {1}", trip.trip_id, trip._Getstop_times_txtRows().Count());
+            //    //foreach (var stop_time in trip._Getstop_times_txtRows())
+            //    //{
+            //    //    Console.WriteLine("{0}, {1}", stop_time.stop_sequence, stop_time.Isarrival_timeNull() ? (TimeSpan?)null : stop_time.arrival_time);
+            //    //}
+            //    //foreach (var path in trip.pathsRow._Getshapes_txtRows())
+            //    //{
+            //    //    Console.WriteLine("{0}, {1}, {2}", path.shape_pt_sequence, path.shape_pt_lat, path.shape_pt_lon);
+            //    //}
+            //}
             gtfs.Write("test2.zip");
         }
     }
